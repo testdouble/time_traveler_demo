3dcb46f0b792efa8c773ff4c7d296fd6b7750b52
Justin Searls <searls@gmail.com>
2022-04-11 22:19:38 -0400

    facepalm emoji

--- db/migrate/20220411155011_create_nowish_function.rb
+++ db/migrate/20220411155011_create_nowish_function.rb
@@ -1,34 +1,30 @@
 class CreateNowishFunction < ActiveRecord::Migration[7.0]
-  def change
-    reversible do |dir|
-      dir.up do
-        # Written to mimic the shape of `pg_catalog.now()':
-        #
-        # CREATE OR REPLACE FUNCTION pg_catalog.now()
-        #  RETURNS timestamp with time zone
-        #  LANGUAGE internal
-        #  STABLE PARALLEL SAFE STRICT
-        # AS $function$now$function$
-        #
-        execute <<~SQL
-          CREATE OR REPLACE FUNCTION public.nowish()
-          RETURNS timestamp with time zone
-          AS
-          $$
-          BEGIN
-          RETURN pg_catalog.now() + (select global_time_offset_seconds
-            from public.system_configurations
-            limit 1
-          ) * interval '1 second';
-          END;
-          $$
-          LANGUAGE plpgsql STABLE PARALLEL SAFE STRICT;
-        SQL
-      end
+  def up
+    # Written to mimic the shape of `pg_catalog.now()':
+    #
+    # CREATE OR REPLACE FUNCTION pg_catalog.now()
+    #  RETURNS timestamp with time zone
+    #  LANGUAGE internal
+    #  STABLE PARALLEL SAFE STRICT
+    # AS $function$now$function$
+    #
+    execute <<~SQL
+      CREATE OR REPLACE FUNCTION public.nowish()
+      RETURNS timestamp with time zone
+      AS
+      $$
+      BEGIN
+      RETURN pg_catalog.now() + (select global_time_offset_seconds
+        from public.system_configurations
+        limit 1
+      ) * interval '1 second';
+      END;
+      $$
+      LANGUAGE plpgsql STABLE PARALLEL SAFE STRICT;
+    SQL
+  end
 
-      dir.down do
-        execute "drop function public.nowish()"
-      end
-    end
+  def down
+    execute "drop function public.nowish()"
   end
 end
